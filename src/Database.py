dd8bd627b34ae1fdebc153d30727b02937543dec
Denis Koshelev <truemacintoshchannel@gmail.com>
2022-08-14 16:32:05 +0200

    basic structure

---
+++ src/Database.py
@@ -0,0 +1,24 @@
+import local_config as config
+#import env_configs as config
+import psycopg2
+
+conn = psycopg2.connect(config.DATABASE_URL, sslmode='require')
+cur = conn.cursor()
+
+
+class Database:
+    def __init__(self, db_url):
+        self.conn = psycopg2.connect(db_url, sslmode='require')
+        self.cur = self.conn.cursor()
+
+    def query(self, query):
+        self.cur.execure(query)
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
+
+    def reviews_in_total(self):
+        cur.execute("SELECT COUNT(*) FROM reviews")
+        row = cur.fetchone()
+        return row[0]
---
+++ src/env_config.py
@@ -0,0 +1,5 @@
+import os
+
+telegram_token = os.environ['telegram_token']
+app_url = os.environ['app_url']
+DATABASE_URL = os.environ['DATABASE_URL']
